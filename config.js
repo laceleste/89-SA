1631dae56bac95786c47f5ccb17325beb8212108
Larissa Celeste <90357832+laceleste@users.noreply.github.com>
2022-12-13 09:38:48 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBwQ30jPsvVUIeuhGFMI_CkjlVrD1XghZU",
+  authDomain: "storytelling-87f8c.firebaseapp.com",
+  databaseURL: "https://storytelling-87f8c-default-rtdb.firebaseio.com",
+  projectId: "storytelling-87f8c",
+  storageBucket: "storytelling-87f8c.appspot.com",
+  messagingSenderId: "352186353165",
+  appId: "1:352186353165:web:fe7b0e9ed09957dbf43744"
+};
